b473efb08132e0b387b92a4943720f3831e116d8
David Reed <reedswork@proton.me>
2022-08-27 15:57:44 -0400

    fix apps

--- src/pages/theatre/apps.tsx
+++ src/pages/theatre/apps.tsx
@@ -1,16 +1,8 @@
 import '../../styles/TheatreCategory.scss';
 import type { HolyPage } from '../../App';
 import TheatreCategory from '../../TheatreCategory';
-import categories from '../../gameCategories';
-import { useSearchParams } from 'react-router-dom';
 
 const Apps: HolyPage = (props) => {
-	const [searchParams] = useSearchParams();
-	const id = searchParams.get('id')!;
-	const category = categories.find((category) => category.id === id);
-
-	if (!category) return <>Bad category ID</>;
-
 	return (
 		<TheatreCategory
 			{...props}
