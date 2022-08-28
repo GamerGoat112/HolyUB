2da402003637d8d0b266956aeeff33e9982ed031
David Reed <reedswork@proton.me>
2022-08-28 14:34:53 -0400

    remove weird FAQ array format

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -3,90 +3,61 @@ import { ObfuscatedThemeA, ThemeLink } from '../ThemeElements';
 import { TN_DISCORD_URL } from '../consts';
 import { Obfuscated } from '../obfuscate';
 import resolveRoute from '../resolveRoute';
-import type { ReactNode } from 'react';
-
-const faq: {
-	q: ReactNode;
-	a: ReactNode;
-}[] = [
-	{
-		q: (
-			<>
-				<Obfuscated>How can I self-host Holy Unblocker?</Obfuscated>
-			</>
-		),
-		a: (
-			<>
-				<Obfuscated>
-					You can self-host/deploy Holy Unblocker by using our all-in-one script
-				</Obfuscated>{' '}
-				<ObfuscatedThemeA href="https://github.com/holy-unblocker/website-aio#website-aio">
-					here
-				</ObfuscatedThemeA>
-				.
-			</>
-		),
-	},
-	{
-		q: <>How do I get more links?</>,
-		a: (
-			<>
-				<Obfuscated>You can join the</Obfuscated>{' '}
-				<ObfuscatedThemeA href={TN_DISCORD_URL}>
-					<Obfuscated>TitaniumNetwork Discord Server</Obfuscated>
-				</ObfuscatedThemeA>{' '}
-				<Obfuscated>
-					to receive more links. In any channel, enter /proxy and select Holy
-					Unblocker.
-				</Obfuscated>
-			</>
-		),
-	},
-	{
-		q: <>Where is this website's source code?</>,
-		a: (
-			<>
-				The source code to this website can be found in our{' '}
-				<ObfuscatedThemeA href="https://git.holy.how/holy/website">
-					Git repository
-				</ObfuscatedThemeA>
-				.
-			</>
-		),
-	},
-	{
-		q: (
-			<>
-				Is my information on the <Obfuscated>proxy</Obfuscated> secure?
-			</>
-		),
-		a: (
-			<>
-				We do not collect any data, your information is only as secure as the
-				sites you are accessing. For privacy concerns, you can review our{' '}
-				<ThemeLink to={resolveRoute('/', 'privacy')}>Privacy Policy</ThemeLink>.
-			</>
-		),
-	},
-];
 
 const FAQ: HolyPage = () => {
-	const sections = [];
-
-	for (let i = 0; i < faq.length; i++) {
-		const { q, a } = faq[i];
-
-		sections.push(
-			<section key={i}>
-				<h1>{q}</h1>
-				<p>{a}</p>
-			</section>
-		);
-	}
-
 	return (
 		<main className="faq">
-			{sections}
+			<section>
+				<h1>
+					<Obfuscated>How can I self-host Holy Unblocker?</Obfuscated>
+				</h1>
+				<p>
+					<Obfuscated>
+						You can self-host/deploy Holy Unblocker by using our all-in-one
+						script
+					</Obfuscated>{' '}
+					<ObfuscatedThemeA href="https://github.com/holy-unblocker/website-aio#website-aio">
+						here
+					</ObfuscatedThemeA>
+					.
+				</p>
+			</section>
+			<section>
+				<h1>How do I get more links?</h1>
+				<p>
+					<Obfuscated>You can join the</Obfuscated>{' '}
+					<ObfuscatedThemeA href={TN_DISCORD_URL}>
+						<Obfuscated>TitaniumNetwork Discord Server</Obfuscated>
+					</ObfuscatedThemeA>{' '}
+					<Obfuscated>
+						to receive more links. In any channel, enter /proxy and select Holy
+						Unblocker.
+					</Obfuscated>
+				</p>
+			</section>
+			<section>
+				<h1>Where is this website's source code?</h1>
+				<p>
+					The source code to this website can be found in our{' '}
+					<ObfuscatedThemeA href="https://git.holy.how/holy/website">
+						Git repository
+					</ObfuscatedThemeA>
+					.
+				</p>
+			</section>
+			<section>
+				<h1>
+					Is my information on the <Obfuscated>proxy</Obfuscated> secure?
+				</h1>
+				<p>
+					We do not collect any data, your information is only as secure as the
+					sites you are accessing. For privacy concerns, you can review our{' '}
+					<ThemeLink to={resolveRoute('/', 'privacy')}>
+						Privacy Policy
+					</ThemeLink>
+					.
+				</p>
+			</section>
 			<p style={{ marginTop: 30, opacity: 0.75 }}>
 				Not what you're looking for?{' '}
 				<ThemeLink to={resolveRoute('/', 'contact')}>Contact Us</ThemeLink>.
